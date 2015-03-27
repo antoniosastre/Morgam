cd45b15e3edb52029024369d723792d0366b2a84
Antonio Sastre <antonio@sastre.es>
2015-03-27 01:29:14 +0100

    Menú "Ir a"

---
+++ goto.php
@@ -0,0 +1,51 @@
+<html>
+	<head>
+	<?php include 'head.php' ?>
+	</head>
+	<body>
+		<?php include 'topmenu.php'; ?>
+		
+<div id="wrapper">
+    <div id="content">
+
+<h1>Ir a...</h1><br>
+
+<table>
+	<tr>
+		<td>
+			Vídeo:
+		</td>
+		<td>
+<form action="video.php" method="get">
+ <input type="text" name="id"> <input type="submit" value="Ir al vídeo">
+</form> 
+</td>
+</tr>
+<tr>
+	<td>
+		Grupo de clips:
+	</td>
+	<td>
+<form action="clipgroup.php" method="get">
+ <input type="text" name="id"> <input type="submit" value="Ir al grupo">
+</form>
+	</td>
+</tr>
+<tr>
+	<td>
+		Clip:
+	</td>
+	<td>
+<form action="clip.php" method="get">
+ <input type="text" name="id"> <input type="submit" value="Ir al clip">
+</form>
+	</td>
+</tr>
+</table>
+
+</div>
+</div>
+
+		
+	</body>
+</html>
